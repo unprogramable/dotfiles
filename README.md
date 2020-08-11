42c744f5f68bd927001e4b7e1ee91adc09dd750c
Unprogrammable <unprogramable@gmail.com>
2020-08-12 04:29:34 +0430

    installer updated

--- README.md
+++ README.md
@@ -3,15 +3,8 @@
 **a simple config for get starting with [neovim/vim] + gruvbox theme**
 
 ## screenshots
+
 ![screenshot1](https://raw.githubusercontent.com/unprogramable/dotfiles/master/screenshots/v1/01.png)
-
-![screenshot2](https://raw.githubusercontent.com/unprogramable/dotfiles/master/screenshots/v1/02.png)
-
-![screenshot3](https://raw.githubusercontent.com/unprogramable/dotfiles/master/screenshots/v1/03.png)
-
-![screenshot4](https://raw.githubusercontent.com/unprogramable/dotfiles/master/screenshots/v1/04.png)
-
-![screenshot5](https://raw.githubusercontent.com/unprogramable/dotfiles/master/screenshots/v1/05.png)
 
 ![screenshot6](https://raw.githubusercontent.com/unprogramable/dotfiles/master/screenshots/v1/06.png)
 
--- install.sh
+++ install.sh
@@ -7,27 +7,27 @@ tick="\033[32m[+]>\033[m"
 
 # 1
 # back up old configs
-echo "$tick backup old configs ~/.vim, ~/.config/nvim"
+echo -e "$tick backup old configs ~/.vim, ~/.config/nvim"
 mv ~/.vim       ~/.vim.bk
 mv ~/.config/nvim ~/.config/nvim.bk
 
 # 2
 # clone git repo
-echo "$tick clone repo: \033[31m${repo}\033[m"
+echo -e "$tick clone repo: \033[31m${repo}\033[m"
 git clone --depth 1 "$repo" ~/.vim
 
 # 3
 # copy vim/neovim config
-if [[ ! $(which vim 2>/dev/null) ]];then
-    echo "$tick set config for vim"
+if [[ $(which vim 2>/dev/null) ]];then
+    echo -e "$tick set config for vim"
     cp "$exm_conf" "~/.vim/init.vim"
     # download extensions
     nvim +PlugInstall +qall
 
 fi
 
-if [[ ! $(which nvim 2>/dev/null) ]];then
-    echo "$tick set config for neovim"
+if [[ $(which nvim 2>/dev/null) ]];then
+    echo -e "$tick set config for neovim"
     ln -s "~/.vim" "~/.config/nvim"
     cp "$exm_conf" "~/.vim/vimrc"
     # download extensions
