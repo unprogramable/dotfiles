652163211636da9b338ee96ad6b5e8c92a2b2acb
Unprogrammable <unprogramable@gmail.com>
2020-08-12 04:36:36 +0430

    new bug fixed

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # variables
 exm_conf="~/.vim/conf.example.vim"
 repo="https://github.com/unprogramable/dotfiles.git"
-tick="\033[32m[+]>\033[m"
+tick="\033[0;32m[+]>\033[1;33m"
 
 # 1
 # back up old configs
@@ -13,23 +13,23 @@ mv ~/.config/nvim ~/.config/nvim.bk
 
 # 2
 # clone git repo
-echo -e "$tick clone repo: \033[31m${repo}\033[m"
+echo -e "$tick clone repo: \033[34m${repo}\033[m"
 git clone --depth 1 "$repo" ~/.vim
 
 # 3
 # copy vim/neovim config
 if [[ $(which vim 2>/dev/null) ]];then
     echo -e "$tick set config for vim"
-    cp "$exm_conf" "~/.vim/init.vim"
+    cp $exm_conf ~/.vim/init.vim
     # download extensions
-    nvim +PlugInstall +qall
+    vim +PlugInstall +qall
 
 fi
 
 if [[ $(which nvim 2>/dev/null) ]];then
     echo -e "$tick set config for neovim"
-    ln -s "~/.vim" "~/.config/nvim"
-    cp "$exm_conf" "~/.vim/vimrc"
+    ln -s ~/.vim ~/.config/nvim
+    cp $exm_conf ~/.vim/vimrc
     # download extensions
     nvim +PlugInstall +qall
 fi
